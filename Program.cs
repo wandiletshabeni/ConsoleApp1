882de1b81cd9ca9c4db39acb3001933e8d8cf484
wandiletshabeni <wandile.tshabeni@gmail.com>
2023-04-18 10:23:46 +0200

    we configured

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ConsoleApp1
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Good Day class");  //we are greeting
+            Console.WriteLine("I love C#"); // we acknowledge the course
+
+            Console.ReadLine();
+        }
+
+    }
+
+}
